f616feb48e55c268ecf9cf0bbb02f3280883fec8
Ollie <olliebyrne2@outlook.com>
2023-08-18 12:46:14 +0100

    added functions to book model and controller

---
+++ controllers/book.js
@@ -0,0 +1,66 @@
+const Book = require("../models/book.js")
+
+
+async function index (req, res) {
+    try {
+        const books = await Book.getAll()
+        res.status(200).json(books)
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+}
+
+async function show (req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const book = await Book.getOneById(id);
+        res.status(200).json(book);
+    } catch (err) {
+        res.status(404).json({ error: err.message })
+    }
+}
+
+
+async function create (req, res) {
+    try {
+        const data = req.body
+        const newBook = await Book.create(data)
+        res.status(201).json(newBook)
+    } catch (err) {
+        res.status(400).json({ error: err.message} )
+    }
+}
+
+async function update (req, res) {
+    try {
+        const data = req.body
+        const id = parseInt(req.params.id)
+        const bookToUpdate = await book.getOneById(id)
+        
+        if (!bookToUpdate) {
+            return res.status(404).send({ message: 'Diary not found' });
+        }
+
+        const result = await bookToUpdate.update(data);
+        res.status(200).json(result);
+
+    } catch (err) {
+        res.status(404).json({error: err.message})
+    }
+}
+
+async function destroy (req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const bookToDelete = await Book.getOneById(id)
+        await bookToDelete.deleteById(id)
+        res.status(204).send({ message: 'book deleted!' })
+    } catch (error) {
+      res.status(404).send({ error: error.message });
+  }
+}
+
+
+module.exports = {
+    index, show, create, update, destroy
+}
---
+++ models/book.js
@@ -0,0 +1,64 @@
+const db = require('../database/connect')
+
+class Book {
+
+    constructor ({ book_id, book_name, book_description, genre, section, checked_out, due_date, overdue, user_details}) {
+        this.id = book_id;
+        this.name = book_name;
+        this.description = book_description;
+        this.genre = genre;
+        this.section = section;
+        this.checked_out = checked_out;
+        this.due_date = due_date;
+        this.overdue = overdue;
+        this.user_details = user_details;
+    }
+
+    static async getAll() {
+        const response = await db.query("SELECT * FROM books;")
+        if (response.rows.length === 0) {
+            throw new Error("No books available.")
+        }
+        return response.rows.map(g => new Book(g))
+    }
+
+
+    static async getOneById(id) {
+        const response = await db.query("SELECT * FROM books WHERE book_id = $1", [id]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate book.")
+        }
+        return new Book(response.rows[0]);
+    }
+
+    static async create(data) {
+        const { name: book_name, description: book_description, genre: genre} = data;
+    
+        const response = await db.query('INSERT INTO books (book_name, book_description, genre) VALUES ($1, $2, $3) RETURNING *;', [book_name, book_description, genre]);
+    
+        return new Book(response.rows[0]);
+    }
+
+    async update(data) {
+        const { name: book_name, description: book_description, genre: genre} = data;
+        const response = await db.query("UPDATE books SET book_name = $1, book_description = $2, genre = $3 WHERE book_id = $4 RETURNING *;", [book_name, book_description, genre, this.id])
+
+        if (response.rows.length != 1) {
+            throw new Error("Unable to update book.")
+        }
+
+        return new Book(response.rows[0]);
+    }
+
+    static async deleteById() {
+        try {
+            await db.query("DELETE FROM books WHERE book_id = $1", [this.id]);
+            return { success: true, message: 'book deleted successfully.'} 
+        } catch (error) {
+            throw new Error('This id does not match an entry')
+        }
+    }
+
+}
+
+module.exports = Book;
