3714866b05b01c5ac7369564918e0d0c75da574e
lanxeyu <lanxeyu@gmail.com>
2023-08-22 17:34:06 +0100

    Modified authenticator requirements

--- routers/admin.js
+++ routers/admin.js
@@ -7,8 +7,9 @@ const adminController = require('../controllers/book.js');
 
 // Admin routes
 adminRouter.get('/', authenticator, adminController.index);
-adminRouter.post('/books/', adminController.create);
-adminRouter.patch('/books/:id', adminController.update);
-adminRouter.delete('/books/:id', adminController.destroy);
+adminRouter.get('/:id', authenticator, adminController.show);
+adminRouter.post('/books/', authenticator, adminController.create);
+adminRouter.patch('/books/:id', authenticator, adminController.update);
+adminRouter.delete('/books/:id', authenticator, adminController.destroy);
 
 module.exports = adminRouter;
