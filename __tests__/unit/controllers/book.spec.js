d2efe8eaeae5d24ae99e1da30de7ac63947aa8b2
lanxeyu <lanxeyu@gmail.com>
2023-08-23 11:18:41 +0100

    Controller tests in progress

--- __tests__/unit/controllers/book.spec.js
+++ __tests__/unit/controllers/book.spec.js
@@ -1,46 +1,27 @@
-const booksController = require('../../../controllers/book')
-const Book = require('../../../models/book')
+const booksController = require('../../../controllers/book');
+const Book = require('../../../models/book');
 
-const mockSend = jest.fn()
-const mockJson = jest.fn()
-const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }))
-const mockRes = { status: mockStatus }
-
+const mockSend = jest.fn();
+const mockJson = jest.fn();
+const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }));
+const mockRes = { status: mockStatus };
 
 describe('books controller', () => {
-  beforeEach(() => jest.clearAllMocks())
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   describe('index', () => {
     it('returns books with a 200 status code', async () => {
-      const testBooks = ['book1', 'book2']
-      jest.spyOn(Book, 'getAll')
-        .mockResolvedValue(testBooks)
+      const testBooks = ['book1', 'book2'];
 
-      await booksController.index(null, mockRes)
-      expect(mockStatus).toHaveBeenCalledWith(200) // good
-      expect(mockSend).toHaveBeenCalledWith({ "data": testBooks })
-    })
+      // Mock the Book.getAll static method
+      jest.spyOn(Book, 'getAll').mockResolvedValue(testBooks);
 
-    // it('calls Colour.getAll', async () => {
-    //   const testColours = { "data": ['c1', 'c2'] }
+      await booksController.index(null, mockRes);
 
-    //   jest.spyOn(Colour, 'getAll')
-    //     .mockResolvedValue(testColours)
-
-    //   await coloursController.index(null, mockRes)
-    //   expect(Colour.getAll).toHaveBeenCalledTimes(1)
-    // })
-
-    // it('rejects', async () => {
-    //   jest.spyOn(coloursController, 'index')
-    //     .mockRejectedValue(new Error('Something happened to your db'))
-
-    //   try {
-    //     await coloursController.index('', mockRes)
-    //   } catch (error) {
-    //     expect(error).toBeTruthy()
-    //     expect(error.message).toBe('Something happened to your db')
-    //   }
-    // })
-  })
-})
+      expect(mockStatus).toHaveBeenCalledWith(200);
+      expect(mockSend).toHaveBeenCalledWith({ "data": testBooks });
+    });
+  });
+});
