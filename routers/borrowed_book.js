742ace35c98dfe11c2617869a511c8bdfe77f1e1
lanxeyu <lanxeyu@gmail.com>
2023-08-24 10:27:49 +0100

    Modified routes

--- routers/borrowed_book.js
+++ routers/borrowed_book.js
@@ -9,7 +9,9 @@ const borrowedBookController = require('../controllers/borrowed_book.js');
 borrowedBookRouter.get('/', authenticator, borrowedBookController.index);
 borrowedBookRouter.get('/:id', borrowedBookController.show);
 borrowedBookRouter.post('/', borrowedBookController.create);
-borrowedBookRouter.patch('/:id', borrowedBookController.update)
-borrowedBookRouter.delete('/:id', borrowedBookController.destroy);
+
+// Unused routes
+// borrowedBookRouter.patch('/:id', borrowedBookController.update)
+// borrowedBookRouter.delete('/:id', borrowedBookController.destroy);
 
 module.exports = borrowedBookRouter;
