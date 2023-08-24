fe6146770072adc33f65b50969359cb093b27e09
lanxeyu <lanxeyu@gmail.com>
2023-08-24 10:26:05 +0100

    Modified routes

--- routers/admin.js
+++ routers/admin.js
@@ -3,10 +3,13 @@ const { Router } = require('express');
 const authenticator = require('../middleware/admin_authenticator.js')
 
 const adminRouter = Router();
-const adminController = require('../controllers/book.js');
+const adminController = require('../controllers/borrowed_book.js');
 
 // Admin routes
 adminRouter.get('/', authenticator, adminController.index);
+adminRouter.get('/borrow', adminController.show);
+adminRouter.get('/borrow/:id', adminController.index);
+
 adminRouter.get('/:id', adminController.show);
 adminRouter.post('/', adminController.create);
 adminRouter.patch('/:id', adminController.update);
